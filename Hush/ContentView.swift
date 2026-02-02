25fe9e13c5183a4b70e93fc78330771519a8c9b8
pakuVision <boardguy.vision@gmail.com>
2026-02-02 18:10:41 +0900

    Initial Commit

---
+++ Hush/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hush
+//
+//  Created by boardguy.vision on 2026/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hush/HushApp.swift
@@ -0,0 +1,17 @@
+//
+//  HushApp.swift
+//  Hush
+//
+//  Created by boardguy.vision on 2026/02/02.
+//
+
+import SwiftUI
+
+@main
+struct HushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
